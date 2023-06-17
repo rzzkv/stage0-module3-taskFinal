c209cae4894e8a7d7a532c54142f151c49704705
rzzkv <rzzkv@yandex.ru>
2023-06-18 02:06:58 +0500

    s

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -3,6 +3,7 @@ package lang.print.gaps.finalModuleTask;
 public class NumbersPrinter {
     public void printIsPositive(int number) {
         boolean isPositive = (number >= 0) && (number != -0);
+
     }
 }
 
